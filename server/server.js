e3106a6f257801ebce18e2d60153c3f00aff34d0
Letlive419 <Letlive419@gmail.com>
2025-01-12 12:51:15 -0500

    Final

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+//** Using express session, cookies and mongodb to handle multiple sessions */
+
 //module imports
 require('dotenv').config();
 const querystring= require("querystring");
@@ -7,18 +9,56 @@ const app = express();
 const axios = require("axios")
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-const scope = "streaming playlist-read-private";
+const scope = "streaming user-top-read user-read-private user-read-email user-library-read user-read-playback-state user-modify-playback-state app-remote-control ";
 const redirect_uri = "http://localhost:8080/callback"
-var token = null
-//Setup
-app.use(cors())
 
-  
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const crypto = require("crypto")
+const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@cluster0.tk2mf.mongodb.net/sessiondb?retryWrites=true&w=majority&appName=Cluster0`
+const MongoStore = require("connect-mongo")
+const mongoose = require("mongoose");
+//Setup
+
+mongoose.connect(mongoURI)
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log('MongoDB connection error:', err));
+
+app.use(cors({
+    origin: "http://localhost:3000", 
+    credentials: true
+}))
+app.use(cookieParser())
+app.use(
+    session({
+        secret:process.env.NODE_SECRET,
+        resave:false,
+        saveUninitialized:true,
+        cookie: {
+            httpOnly:true,
+            maxAge:3600000
+        },
+        store: MongoStore.create({
+            mongoUrl:mongoURI,
+            collectionName:"users",
+            ttl: 14*24*60*60,
+        })
+    })
+)
+
+
+
+function generateRandomId(length = 16) {
+    return crypto.randomBytes(length).toString("base64").slice(0,length)
+}
 
 const PORT = process.env.PORT || 8080;
 //routes
 
-app.get("/home",(req, res) => {
+//user enters the home page 
+app.get("/home",async(req, res) => {
+    
+    
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
           response_type: 'code',
@@ -27,10 +67,12 @@ app.get("/home",(req, res) => {
           redirect_uri: redirect_uri,
         }));
 })
-
+//home redirects the user to spotify and is sent to this callback
 app.get("/callback", async(req, res) => {
-    const code = req.query.code || null;
     
+    const code = req.query.code || null;
+    const sessionID = req.sessionID;
+   
     if (code) {
         const response = await axios.post("https://accounts.spotify.com/api/token",
         querystring.stringify({
@@ -43,14 +85,47 @@ app.get("/callback", async(req, res) => {
                 "content-type":"application/x-www-form-urlencoded",
                 Authorization: "Basic "+ (new Buffer.from(client_id + ":" + client_secret).toString("base64"))}}
     )
-   token = response.data;
+   const token = await response.data.access_token;
+   //callback redirects the user back to their domain
+   await mongoose.connection.db.collection("users").updateOne({
+    _id:sessionID},
+    {$set: {"token":token}
+   })
    res.redirect("http://localhost:3000")
-  
+   
 }
 })
 
-app.get("/token", (req, res) => {
-    res.send(token)
+//user retrieves the token used
+app.get("/token", async(req, res) => {
+    
+    
+    try {
+        const session = await mongoose.connection.db.collection("users").findOne({_id: req.sessionID});
+        
+        if (!session || !session.token) {
+            return
+        }
+        else {
+            
+            res.send(session.token)
+        }
+    } catch(exception) {
+        
+        res.send(exception)
+    }
+})
+
+//user uses token to obtain tracks
+app.get("/tracks", async(req, res) => {
+    
+    const session = await mongoose.connection.db.collection("users").findOne({_id: req.sessionID});
+   
+    const requestToken = session.token;
+    const response = await axios.get("https://api.spotify.com/v1/me/top/tracks?limit=10",{ headers: {
+        Authorization: `Bearer ${requestToken}`
+    }})
+    res.send(response.data)
 })
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,194 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import querystring from "querystring";
+
+
 
 
 
 function Home() {
-  const client_id = process.env.REACT_APP_CLIENT_ID;
-  var redirect_uri = "http://localhost:8080/callback"
-  var scope = "user-read-private";
-    
+  
+  const [token, setToken] = useState(null);
+  const [items, setItems] = useState([])
+  const playerRef = useRef(null);  // Store the player instance
+  const [deviceID, setdeviceID] = useState(null)
+  const [volume, setVolume] = useState(.05)
+  const [title, setTitle] = useState("Please connect to Spotify via Spotify Connect")
+  
     useEffect(() => {
-       
-         
-    },[])
- 
-    function handleTop5() {
+        const getToken = async() => {
+        const response = await axios.get("http://localhost:8080/token", {withCredentials: true})
+        setToken(response.data)
+        }    
+        if(!token) {
+          getToken()
+        }
+    }, [])
+
+
+   useEffect(() => {
+    if (token) {
+    
+      const script = document.createElement("script");
+      script.src = "https://sdk.scdn.co/spotify-player.js";
+      script.onload = () => { console.log(token)};
+      document.head.appendChild(script)
+      if (!playerRef.current) {
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        console.log("mounted spotify web playback")
+        const usertoken = token;
+        const player = new window.Spotify.Player({
+          name: 'Web Playback SDK',
+          getOAuthToken: cb => { cb(usertoken); },
+          volume: 0.5
+      })
+
+     // Ready event
+     player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+        setdeviceID(device_id);
+        playerRef.current = player;  // Store player in ref
+    });
+
+    // Not Ready event
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+
+    // Error listeners
+    player.addListener('initialization_error', ({ message }) => {
+      console.error('Initialization Error:', message);
+    });
+    player.addListener('authentication_error', ({ message }) => {
+      console.error('Authentication Error:', message);
+    });
+    player.addListener('account_error', ({ message }) => {
+      console.error('Account Error:', message);
+    });
+    player.addListener('player_state_changed', ({track_window}) => {
+      if (track_window) {
+        setTitle(track_window.current_track.name)
+        console.log(track_window)
+      }
+    })
+  
+    // Connect the player
+    player.connect();
+
+    
+
+    
+  }
+  };
+}
+   },[token])
+
+   
+  
+    function login() {
       window.location.href = "http://localhost:8080/home";
     }
 
     async function getTracks() {
-      const response = await axios.get("http://localhost:8080/token")
-      console.log(response)
+      const response = await axios.get("http://localhost:8080/tracks",{withCredentials: true})
+      console.log("Get Tracks, Your doing great!")
+      if (items.length < 5) {
+        const newItems = response.data.items?.map((tracks) => (
+          {key: tracks.id, name: tracks.name, uri: tracks.uri}
+        ))
+        
+        setItems([...items, ...newItems])
+       
+      }
+      
+    
+        
+      
     }
   
+    function handleVolumeChange(event) {
+      const newVolume = parseFloat(event.target.value);
+      setVolume(newVolume);
+      if (playerRef.current) {
+        playerRef.current.setVolume(newVolume)
+      } else {
+        console.log("Player not ready")
+      }
+    };
+
+    async function handlePlay(uri,title) {
+      
+        if (playerRef.current) {
+              await axios.put(`https://api.spotify.com/v1/me/player/play?device_id=${deviceID}`, {uris: [uri]},
+              {headers: 
+              {Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json',
+        }}
+        
+        );
+        setTitle(title)
+        console.log("track playing")
+                
+            } else {
+              console.log("Player is not ready");
+            }
+      
+      
+    }
+    async function handleResume() { 
+      if (playerRef.current) {
+        await playerRef.current.resume() 
+      }
+      else {
+        console.log("Player not ready")
+      }
+    }
+    async function handlePause(){
+      
+        if (playerRef.current) {
+          await playerRef.current.pause()
+        }
+        else {
+          console.log("Player not ready")
+        }
+    }
+
+ 
+    
+
   return (
     <div>
       <h1>Music Spotify API</h1>
-      <button onClick={handleTop5}>Login</button>
-      <button onClick={getTracks}>Get Tracks</button>
+      {!token ? <button onClick={login}>Login</button> : <button onClick={getTracks}>Get Tracks</button> }
+      <h1>Top Tracks!</h1>
+      {items.map((item) => {
+        return(
+        <div>
+        <button id={item.uri} onClick={() =>handlePlay(item.uri, item.name) }>{item.name}</button> 
+        
+        
+        </div>
+
+        )
+
+      })}
+
+      
+      {(items.length > 1) ?
+      
+        <div>
+        <h1> {title} </h1>
+        <button  onClick={async () => {
+          if (playerRef.current) {
+            await playerRef.current.togglePlay();
+          }
+        }}>Play Song</button>
+        <button  onClick={handlePause}>Pause</button>
+        <button  onClick={handleResume}>Resume</button>
+        <input type="range" value={volume} min={0} max={1} step={.01} onChange={handleVolumeChange}></input>
+        </div>
+       :
+       console.log("Nothing to display") 
+      }
     </div>
   );
 }
